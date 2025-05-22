09f46bd93da24a1d0660fe20af794cb2d7eb7772
BLoST53199 <blosttmb@gmail.com>
2025-05-22 21:48:12 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -34,7 +34,7 @@ def webhook():
     bot.process_new_updates([update])
     return "OK", 200
 
-if name == "__main__":
+if __name__ == "__main__":
     bot.remove_webhook()
     bot.set_webhook(url=f"{WEBHOOK_URL}/{TELEGRAM_BOT_TOKEN}")
     port = int(os.environ.get("PORT", 5000))
